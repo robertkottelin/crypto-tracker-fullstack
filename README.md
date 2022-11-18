2633cd4878d8dd464ad95233e1209b5effbd11d0
robertkottelin <robert.kottelin@gmail.com>
2022-11-18 15:57:54 +0100

    backend implemented, new repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cryptocurrency Tracker with React JS, Material UI and Chart JS. Backend implemented with Firebase
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD8kp1xaRZihKU9eVzUGyBJwqG4TwxsJV0",
+  authDomain: "crypto-tracker-2-cc824.firebaseapp.com",
+  projectId: "crypto-tracker-2-cc824",
+  storageBucket: "crypto-tracker-2-cc824.appspot.com",
+  messagingSenderId: "17126242170",
+  appId: "1:17126242170:web:e9e6bc93b1f9ec0c5e48c3"
+};
+export default firebaseConfig;
